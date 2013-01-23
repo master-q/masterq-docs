d7a0ad2de79c480823b7db397554f9b360105cc0
Kiwamu Okabe <kiwamu@debian.or.jp>
2013-01-23 12:10:11 +0900

    Rewrite snatched bus_space function.

--- presentations/20130125_what_is_metasepi/SnatchEhciIntr.hs
+++ presentations/20130125_what_is_metasepi/SnatchEhciIntr.hs
@@ -18,53 +18,30 @@ type Addr = Int
 ehciUsbsts, ehciUsbIntr :: Addr
 ehciUsbsts  = 0x04
 ehciUsbIntr = 0x08
-
 ehciStsIaa, ehciStsPcd, ehciStsErrInt, ehciStsInt :: Int
 ehciStsIaa    = 0x00000020
 ehciStsPcd    = 0x00000004
 ehciStsErrInt = 0x00000002
 ehciStsInt    = 0x00000001
 
-busSpaceOread4 :: Addr -> BusSpace m Int
-busSpaceOread4 = undefined
-busSpaceOwrite4 :: Addr -> Int -> BusSpace m ()
-busSpaceOwrite4 = undefined
-
-ehciStsIntrs :: Int -> Int
-ehciStsIntrs r = r .&. 0x3f
-
-evalWakeup :: Int -> BusSpace IO Int
-evalWakeup ei | ei .&. flag /= 0 = go
-              | otherwise = return ei
-  where flag = ehciStsIaa
-        go = do sc <- get
-                liftIO . wakeUp . scAsyncHead $ sc
+evalTmpl :: Int -> (SoftContext -> IO a) -> Int -> BusSpace IO Int
+evalTmpl flag io ei | ei .&. flag /= 0 = go
+                    | otherwise = return ei
+  where go = do sc <- get
+                liftIO . io $ sc
                 return $ ei .&. complement flag
 
-evalSoftIntr :: Int -> BusSpace IO Int
-evalSoftIntr ei | ei .&. flag /= 0 = go
-                | otherwise = return ei
-  where flag = ehciStsErrInt .|. ehciStsInt
-        go = do sc <- get
-                liftIO . usbSchedSoftIntr . scBus $ sc
-                return $ ei .&. complement flag
+evalWakeup, evalSoftIntr, evalPcd :: Int -> BusSpace IO Int
+evalWakeup = evalTmpl ehciStsIaa $ wakeUp . scAsyncHead
+evalSoftIntr = evalTmpl (ehciStsErrInt .|. ehciStsInt) (usbSchedSoftIntr . scBus)
+evalPcd = evalTmpl ehciStsPcd (\sc -> ehciPcd sc . scIntrXfer $ sc)
 
-evalPcd :: Int -> BusSpace IO Int
-evalPcd ei | ei .&. flag /= 0 = go
-           | otherwise = return ei
-  where flag = ehciStsPcd
-        go = do sc <- get
-                liftIO . ehciPcd sc . scIntrXfer $ sc
-                return $ ei .&. complement flag
-
-evalWrite :: Int -> BusSpace IO Int
-evalWrite ei | ei /= 0 = go
-             | otherwise = return ei
+evalWrite :: Int -> BusSpace IO ()
+evalWrite ei = when (ei /= 0) go
   where go = do sc <- get
                 let newEi = scEintrs sc .&. complement ei
                 put $ sc {scEintrs = newEi}
                 busSpaceOwrite4 ehciUsbIntr newEi
-                return newEi
 
 evaluateIntr1 :: BusSpace IO ()
 evaluateIntr1 =
@@ -74,6 +51,7 @@ evaluateIntr1 =
      busSpaceOwrite4 ehciUsbsts eintrs
      evalWrite =<< evalPcd =<< evalSoftIntr =<< evalWakeup eintrs
      return ()
+       where ehciStsIntrs r = r .&. 0x3f
 
 ehciIntr1 :: SoftContext -> IO (Either () SoftContext)
 ehciIntr1 sc = return . Right =<< execStateT evaluateIntr1 sc
@@ -84,3 +62,7 @@ usbSchedSoftIntr :: UsbdBus -> IO ()
 usbSchedSoftIntr = undefined
 ehciPcd :: SoftContext -> Ptr Int -> IO ()
 ehciPcd = undefined
+busSpaceOread4 :: Addr -> BusSpace m Int
+busSpaceOread4 = undefined
+busSpaceOwrite4 :: Addr -> Int -> BusSpace m ()
+busSpaceOwrite4 = undefined
